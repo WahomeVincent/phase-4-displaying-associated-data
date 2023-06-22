10c3bdfd5de40e5285826bf9b94c4d622d3510cf
Wahome <vinwahome@gmail.com>
2023-06-22 15:06:03 +0300

    Nesting Routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,13 @@
 class ReviewsController < ApplicationController
 
   def index
-    reviews = Review.all.order(rating: :desc)
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all.order(rating: :desc)
+    end
+    
     render json: reviews, include: :dog_house
   end
 
